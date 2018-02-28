1623422c8e967d3d8bac662debbcbb4cb0b39e88
Joshua Gardner <mellowcellofellow@gmail.com>
2018-02-27 18:04:34 -0700

    Remove cabinetgroup, cabinet, and drawer models
    Been replaced by Location model

--- library/admin.py
+++ library/admin.py
@@ -1,13 +1,11 @@
 from django.contrib import admin
 
-from .models import (Artist, Piece, ScoreType, CabinetGroup,
-                     Cabinet, Drawer, Orchestra, Performance)
+from .models import (Artist, Piece, ScoreType, Location, Orchestra,
+                     Performance)
 
 admin.site.register(Artist)
 admin.site.register(Piece)
 admin.site.register(ScoreType)
-admin.site.register(CabinetGroup)
-admin.site.register(Cabinet)
-admin.site.register(Drawer)
+admin.site.register(Location)
 admin.site.register(Orchestra)
 admin.site.register(Performance)
--- library/models.py
+++ library/models.py
@@ -27,9 +27,7 @@ class Piece(models.Model):
     )
 
     id = models.IntegerField('Catalog ID Number', primary_key=True)
-    drawer = models.ForeignKey('Drawer', models.PROTECT)
-    location = models.ForeignKey('Location', models.PROTECT,
-                                  null=True)
+    location = models.ForeignKey('Location', models.PROTECT)
     title = models.CharField('Title', max_length=256)
     subtitle = models.CharField('Subtitle (Optional)', max_length=128,
                                 blank=True)
@@ -92,71 +90,6 @@ class Location(models.Model):
         unique_together = (
             ('identifier', 'parent'),
         )
-
-
-class CabinetGroup(models.Model):
-    shortname = models.CharField('Short Name', max_length=5, unique=True)
-    description = models.CharField('Description', max_length=140)
-
-    def __str__(self):
-        return "%s" % self.shortname
-
-    def get_absolute_url(self):
-        return reverse('library.views.group_list',
-                       kwargs={'group_name': self.shortname})
-
-    def get_edit_url(self):
-        return reverse('admin', args=['library/cabinetgroup/%d/' % self.id])
-
-    class Meta:
-        ordering = ['shortname']
-
-
-class Cabinet(models.Model):
-    number = models.IntegerField('Cabinet ID Number')
-    group = models.ForeignKey('CabinetGroup', models.PROTECT)
-
-    def __str__(self):
-        return "%s >> %s" % (self.group, self.number)
-
-    def __int__(self):
-        return self.number
-
-    def get_absolute_url(self):
-        return reverse('library.views.cabinet_list', kwargs={
-            'group_name': self.group.shortname,
-            'cabinet_id': str(int(self)),
-        })
-
-    def get_edit_url(self):
-        return reverse('admin', args=['library/cabinet/%d/' % self.id])
-
-    class Meta:
-        ordering = ['group', 'number']
-
-
-class Drawer(models.Model):
-    cabinet = models.ForeignKey('Cabinet', models.PROTECT)
-    number = models.SmallIntegerField('Drawer Number')
-
-    def __str__(self):
-        return "%s >> %s" % (self.cabinet, self.number)
-
-    def __int__(self):
-        return self.number
-
-    def get_absolute_url(self):
-        return reverse('library.views.drawer_list', kwargs={
-            'group_name': self.cabinet.group,
-            'cabinet_id': str(int(self.cabinet)),
-            'drawer_id': str(int(self))
-        })
-
-    def get_edit_url(self):
-        return reverse('admin', args=['library/drawer/%d/' % self.id])
-
-    class Meta:
-        ordering = ['cabinet', 'number']
 
 
 class Orchestra(models.Model):
